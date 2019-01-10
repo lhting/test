959c617b087a991b509ec9e66dbbf7fe9a260a5c
lhting <lucas.ting@gmail.com>
2019-01-10 15:22:55 -0800

    Changed App.py

--- App.py
+++ App.py
@@ -1,7 +1,7 @@
 #This is a temporary app to learn git on
 
 def printme():
-    string = input("What should I print?!")
+    string = input("What should I print?!?")
     print(string)
     
 printme()
